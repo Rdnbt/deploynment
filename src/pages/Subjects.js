5b92966083a7fe018e82ba1293d29407fec6c21e
Rdnbt <haroldbudd12@gmail.com>
2023-12-11 23:55:50 +0900

    fixed simple bug

--- src/pages/Subjects.js
+++ src/pages/Subjects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Card from '../components/Card';
-import './Subjects.css';
+import './subjects.css';
 
 function Subjects() {
   const categories = {
